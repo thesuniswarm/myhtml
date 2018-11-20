0e858566fb082190c45e03f5f709534a6c9692cd
thesuniswarm <44889891+thesuniswarm@users.noreply.github.com>
2018-11-20 12:08:03 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,21 +1,21 @@
 var r,score=0;
 function rock() {
 	
-	document.getElementById("myChoice").innerHTML="<img src='./1.jpg'>";
+	document.getElementById("myChoice").innerHTML="<img src='./images/1.jpg'>";
 	
 	judge("rock");
 }
 
 function scissors() {
 	
-	document.getElementById("myChoice").innerHTML="<img src='./2.jpg'>";
+	document.getElementById("myChoice").innerHTML="<img src='./images/2.jpg'>";
 	
 	judge("scissors");
 }
 
 function paper	() {
 	
-	document.getElementById("myChoice").innerHTML="<img src='./3.jpg'>";
+	document.getElementById("myChoice").innerHTML="<img src='./images/3.jpg'>";
 	
 	judge("paper");
 }
@@ -73,15 +73,15 @@ function go(){
 
 function computerChoice() {
 	if(r<1){
-		document.getElementById("computerChoice").innerHTML="<img src='./1.jpg'>";
+		document.getElementById("computerChoice").innerHTML="<img src='./images/1.jpg'>";
 		return "rock";
 	}
 	else if (r<2) {
-		document.getElementById("computerChoice").innerHTML="<img src='./2.jpg'>";
+		document.getElementById("computerChoice").innerHTML="<img src='./images/2.jpg'>";
 		return "scissors";
 	}
 	else{
-		document.getElementById("computerChoice").innerHTML="<img src='./3.jpg'>";
+		document.getElementById("computerChoice").innerHTML="<img src='./images/3.jpg'>";
 	    return "paper";
 	}
 }
